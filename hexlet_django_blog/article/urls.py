68359f379ba91904496366a55563b9182a4ba54f
Igor Cherkashov <cherkashov270999@gmail.com>
2024-06-30 22:35:29 +0300

    Маршрутизация

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -4,4 +4,5 @@ from hexlet_django_blog.article import views
 
 urlpatterns = [
     path('', views.Index.as_view()),
+    path('<str:tags>/<int:article_id>/', views.med_info_view, name='article')
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -3,4 +3,7 @@ from django.views import View
 
 class Index(View):
     def get(self, request):
-        return HttpResponse('article', context={'name': 'article'})
+        return reverse('article', kwargs={'tags': python, 'article_id': 42})
+
+def med_info_view(request, tags, article_id):
+    return HttpResponse(f'Статья номер {article_id}. Тег {tags}')
